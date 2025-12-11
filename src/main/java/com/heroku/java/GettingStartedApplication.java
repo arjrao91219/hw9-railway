76b65168816181891c521263af42946a3260bf23
Ahmad Rao <ahmadrao@u.boisestate.edu>
2025-12-11 13:00:58 -0700

    Change /database to /db

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -26,7 +26,7 @@ public class GettingStartedApplication {
         return "index";
     }
 
-    @GetMapping("/database")
+    @GetMapping("/db")
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
